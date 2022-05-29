d0fe02aa615e37080efe295006c2c12d9b27f945
John Obaje <67030349+lilobaje@users.noreply.github.com>
2022-05-29 16:41:21 -0700

    update

--- Poll_web_app/manage.py
+++ Poll_web_app/manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Poll_web_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
